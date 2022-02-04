3f7950c7f99dae1c58a719ba91c71d270409f20b
Igna Garcia <gnachoxp@gmail.com>
2022-02-04 19:41:09 -0300

    init api documentation

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 from fastapi import FastAPI
 from uuid import uuid4
-from models import User
+from models import User, UserUpdateReq, Role
 from mock import users
 
-app = FastAPI()
-base = "/api"
+tags_metadata = [
+    {
+        "name": "user",
+        "description": "Operations with users."
+    }
+]
 
-@app.get('/api/user')
-def getUsers():
+app = FastAPI(
+    title = "First API",
+    description = "Firt API with FastAPI.",
+    contact = {
+        "name": "Igna Garcia",
+        "url": "https://ignagarcia.vercel.app/",
+        "email": "gnachoxp@gmail.com"
+    },
+    openapi_tags = tags_metadata,
+    redoc_url=None
+)
+
+
+@app.get('/')
+async def root():
+    return {
+        "api": "/api", 
+        "docs": "/docs"
+    }
+
+@app.get('/api/user', tags=["user"])
+async def list_users():
     return {
         "message": "succes", 
         "user": users
     }
     
-@app.get('/api/user/{user_id}')
-def getUserById(user_id: str):
+@app.get('/api/user/{user_id}', tags=["user"])
+async def get_user_by_id(user_id: str, summary="Find User by User UUID"):
     for user in users:
         if user['id'] == user_id:
-            return {
-                "message": "succes", 
-                "user": user
-            }
+            return user
     raise HTTPException(status_code=404, detail=f"User({user_id}) Not Found")
 
-@app.post('/api/user')
-def createUser(user: User):
+@app.post('/api/user', tags=["user"])
+async def create_user(user: User):
     user.uuid = str(uuid4())
     users.append(user.dict())
     return {
@@ -32,20 +53,28 @@ def createUser(user: User):
         "user": users
     }
     
-@app.put('/api/user/{user_id}')
-def updateUser(user_updated: User, user_id: str):
+@app.put('/api/user/{user_id}', tags=["user"], response_model=User, summary="Change values of User")
+async def update_user(user_updated: UserUpdateReq, user_id: str):
+    """
+    Update an user with passed values:
+
+    - **name**: each user must have a name
+    - **age**: age of the user
+    - **role**: [admin, user]
+    """
     for user in users:
         if user['id'] == user_id:
-            user["name"] = user_updated.name
-            user["age"] = user_updated.age
-            return {
-                "message": "succes", 
-                "user": user
-            }
+            if user_updated.name is not None:
+                user["name"] = user_updated.name
+            if user_updated.age is not None:
+                user["age"] = user_updated.age
+            if user_updated.role is not None:
+                user["role"] = user_updated.role
+            return user
     raise HTTPException(status_code=404, detail=f"User({user_id}) Not Found")
     
-@app.delete('/api/user/{user_id}')
-def deleteUser(user_id: str):
+@app.delete('/api/user/{user_id}', tags=["user"], response_model=User)
+async def delete_user(user_id: str):
     for idx,user in enumerate(users):
         if user['id'] == user_id:
             return {
--- models.py
+++ models.py
@@ -1,7 +1,18 @@
 from pydantic import BaseModel
 from typing import Optional
+from enum import Enum
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
 
 class User(BaseModel):
     uuid: Optional[str]
     name: str
     age: int
+    role: Role
+    
+class UserUpdateReq(BaseModel):
+    name: Optional[str]
+    age: Optional[int]
+    role: Optional[Role]
